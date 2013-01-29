3f6f9020f3df580100ded6ea585a0b7a9d94c8e1
tonyhuang <tonyh1986@gmail.com>
2013-01-29 13:12:12 -0800

    improve trim and substr tests

--- src/test/java/com/salesforce/phoenix/end2end/VariableLengthPKTest.java
+++ src/test/java/com/salesforce/phoenix/end2end/VariableLengthPKTest.java
@@ -128,7 +128,7 @@ public class VariableLengthPKTest extends BaseClientMangedTimeTest {
         
         stmt.setString(1, "   def");
         stmt.execute();
-        stmt.setString(1, "jkl...");
+        stmt.setString(1, "jkl   ");
         stmt.execute();
         stmt.setString(1, "   ghi   ");
         stmt.execute();
@@ -922,6 +922,7 @@ public class VariableLengthPKTest extends BaseClientMangedTimeTest {
             "SELECT substr('ĎďĒ',2,2) FROM BTABLE LIMIT 1",
             "SELECT substr('ĎďĒ',-1,1) FROM BTABLE LIMIT 1",
             "SELECT substr('Ďďɚʍ',2,4) FROM BTABLE LIMIT 1",
+            "SELECT A_STRING FROM BTABLE WHERE substr(A_STRING, 0, 3)='jkl'",
         };
         String result[] = {
             "C",
@@ -937,6 +938,7 @@ public class VariableLengthPKTest extends BaseClientMangedTimeTest {
             "ďĒ",
             "Ē",
             "ďɚʍ",
+            "jkl   ",
         };
         assertEquals(query.length,result.length);
         String url = PHOENIX_JDBC_URL + ";" + PhoenixRuntime.CURRENT_SCN_ATTRIB + "=" + (ts + 5); // Run query at timestamp 5
@@ -1362,6 +1364,7 @@ public class VariableLengthPKTest extends BaseClientMangedTimeTest {
         try {
             initTableValues(null, ts);
             for (int i = 0; i < query.length; i++) {
+                System.out.println(query[i]);
                 PreparedStatement statement = conn.prepareStatement(query[i]);
                 ResultSet rs = statement.executeQuery();
                 assertTrue(rs.next());
