7ce01df2257a0c35a7dadc1bdad0dadb3575eb2d
tonyhuang <tonyh1986@gmail.com>
2013-01-29 15:14:03 -0800

    removed a system.out.println

--- src/test/java/com/salesforce/phoenix/end2end/VariableLengthPKTest.java
+++ src/test/java/com/salesforce/phoenix/end2end/VariableLengthPKTest.java
@@ -1371,7 +1371,6 @@ public class VariableLengthPKTest extends BaseClientMangedTimeTest {
         try {
             initTableValues(null, ts);
             for (int i = 0; i < query.length; i++) {
-                System.out.println(query[i]);
                 PreparedStatement statement = conn.prepareStatement(query[i]);
                 ResultSet rs = statement.executeQuery();
                 assertTrue(rs.next());
