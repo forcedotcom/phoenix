ea4780be9e7347a546e0507129152d552a6d95db
ryang-sfdc <ryang.sfdc@gmail.com>
2013-03-12 08:56:18 -0700

    start writing skip scan tests

--- src/test/java/com/salesforce/phoenix/filter/SkipScanFilterTest.java
+++ src/test/java/com/salesforce/phoenix/filter/SkipScanFilterTest.java
@@ -1,42 +1,92 @@
 package com.salesforce.phoenix.filter;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import junit.framework.TestCase;
-import org.apache.hadoop.hbase.filter.Filter;
-import org.apache.hadoop.hbase.filter.FilterBase;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.filter.*;
+import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp;
+import org.apache.hadoop.hbase.filter.Filter.ReturnCode;
 import org.apache.hadoop.hbase.util.Bytes;
 
+
+//reset()
+//filterAllRemaining() -> true indicates scan is over, false, keep going on.
+//filterRowKey(byte[],int,int) -> true to drop this row, if false, we will also call
+//filterKeyValue(KeyValue) -> true to drop this key/value
+//filterRow(List) -> allows directmodification of the final list to be submitted
+//filterRow() -> last chance to drop entire row based on the sequence of filterValue() calls. Eg: filter a row if it doesn't contain a specified column.        
 public class SkipScanFilterTest extends TestCase {
     SkipScanFilter skipper = new SkipScanFilter();
-    Filter inner = new FilterBaseExtension();
     
     public void testSkip() {
         List<List<byte[]>> cnf =
             Arrays.asList(
                 Arrays.asList(
-                        Bytes.toBytes("abc"), Bytes.toBytes("def")
+                    Bytes.toBytes("abc"), Bytes.toBytes("def")
                 )
             );
                 
-        skipper.setFilter(new FilterBaseExtension()).setCnf(cnf);
+        Filter filter = new RowFilter(CompareOp.EQUAL, new BinaryComparator(Bytes.toBytes("abd")));
+        skipper.setFilter(filter).setCnf(cnf);
         
-        assertFalse(skipper.filterAllRemaining());
-    }
-    
-    private final class FilterBaseExtension extends FilterBase {
-        @Override public boolean filterRowKey(byte[] buffer, int offset, int length) {
-            return super.filterRowKey(buffer, offset, length);
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("abb"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.SEEK_NEXT_USING_HINT, skipper.filterKeyValue(kv));
+            assertEquals(KeyValue.createFirstOnRow(Bytes.toBytes("abc")), skipper.getNextKeyHint(kv));
+        }        
+        
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("ab"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.SEEK_NEXT_USING_HINT, skipper.filterKeyValue(kv));
+            assertEquals(KeyValue.createFirstOnRow(Bytes.toBytes("abc")), skipper.getNextKeyHint(kv));
         }
-        @Override public void readFields(DataInput in) throws IOException {
-            throw new UnsupportedOperationException();
+        
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("abc"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.NEXT_ROW, skipper.filterKeyValue(kv));
         }
-        @Override public void write(DataOutput out) throws IOException {
-            throw new UnsupportedOperationException();
+        
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("abd"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.INCLUDE, skipper.filterKeyValue(kv));
         }
-    }
 
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("abe"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.NEXT_ROW, skipper.filterKeyValue(kv));
+        }
+
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("def"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.NEXT_ROW, skipper.filterKeyValue(kv));
+        }
+        
+        
+        {
+            KeyValue kv = KeyValue.createFirstOnRow(Bytes.toBytes("deg"));
+            skipper.reset();
+            assertFalse(skipper.filterAllRemaining());
+            assertFalse(skipper.filterRowKey(kv.getBuffer(), kv.getOffset(), kv.getLength()));
+            assertEquals(ReturnCode.NEXT_ROW, skipper.filterKeyValue(kv));
+        }
+}
 }
