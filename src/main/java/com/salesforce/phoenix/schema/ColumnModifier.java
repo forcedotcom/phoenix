3cc1a263a8471a9950c6b3525805e0b7f7ba161d
Simon Toens <stoens@salesforce.com>
2013-04-16 17:50:15 -0700

    Update comments.

--- src/main/java/com/salesforce/phoenix/schema/ColumnModifier.java
+++ src/main/java/com/salesforce/phoenix/schema/ColumnModifier.java
@@ -1,11 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Salesforce.com, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *     Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *     Neither the name of Salesforce.com nor the names of its contributors may 
+ *     be used to endorse or promote products derived from this software without 
+ *     specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 package com.salesforce.phoenix.schema;
 
 import com.google.common.base.Preconditions;
 import com.sun.istack.NotNull;
 import com.sun.istack.Nullable;
 
+/**
+ * A ColumnModifier implementation modifies how bytes are stored in a primary key column.</p>  
+ * The {@link ColumnModifier#apply apply} method is called when the bytes for a specific column are first written to HBase and again
+ * when they are read back.  Phoenix attemps to minimize calls to apply when bytes are read out of HBase.   
+ * 
+ * @author stoens
+ * @since 1.2
+ */
 public enum ColumnModifier {
 	
+    /**
+     * Invert the bytes in the src byte array to support descending ordering of row keys.
+     */
 	SORT_DESC() {
         @Override
 		public byte[] apply(byte[] src, byte[] dest, int offset, int length) {
@@ -55,7 +93,7 @@ public enum ColumnModifier {
 	}
 	
 	/**
-	 * Copy the bytes from the src array to the dest array and apply the column modifier operation on the bytes
+	 * Copy the bytes from src array to dest array and apply the column modifier operation on the bytes
 	 * starting at the specified offset index.  The column modifier is applied to the number of bytes matching the 
 	 * specified length.  If dest is null, a new byte array is allocated.
 	 * 
