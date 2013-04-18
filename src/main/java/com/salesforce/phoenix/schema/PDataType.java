0ff8430927b1f007d5b4a8714f45752a4bab87de
Simon Toens <stoens@salesforce.com>
2013-04-17 19:32:18 -0700

    Fix tests.

--- src/main/java/com/salesforce/phoenix/schema/PDataType.java
+++ src/main/java/com/salesforce/phoenix/schema/PDataType.java
@@ -1977,14 +1977,17 @@ public enum PDataType {
         return true;
     }
 
-    public int compareTo(ImmutableBytesWritable ptr1, ColumnModifier ptr1ColumnModifier, ImmutableBytesWritable ptr2, ColumnModifier ptr2ColumnModifier) {
-        return compareTo(ptr1.get(), ptr1.getOffset(), ptr1.getLength(), ptr1ColumnModifier, ptr2.get(), ptr2.getOffset(), ptr2.getLength(), ptr2ColumnModifier);
+    public int compareTo(byte[] b1, byte[] b2) {
+        return compareTo(b1, 0, b1.length, null, b2, 0, b2.length, null);
+    }
+
+    public int compareTo(ImmutableBytesWritable ptr1, ImmutableBytesWritable ptr2) {
+        return compareTo(ptr1.get(), ptr1.getOffset(), ptr1.getLength(), null, ptr2.get(), ptr2.getOffset(), ptr2.getLength(), null);
     }
 
     public int compareTo(byte[] b1, int offset1, int length1, ColumnModifier mod1, byte[] b2, int offset2, int length2, ColumnModifier mod2) {
         int resultMultiplier = -1;
-        boolean invertResult = (mod1 == mod2 && mod1 == ColumnModifier.SORT_DESC); 
-        
+        boolean invertResult = (mod1 == mod2 && mod1 == ColumnModifier.SORT_DESC);
         if (!invertResult) {
             if (mod1 != null) {
                 b1 = mod1.apply(b1, null, offset1, length1);
@@ -1992,12 +1995,15 @@ public enum PDataType {
             if (mod2 != null) {
                 b2 = mod2.apply(b2, null, offset2, length2);
             }            
-            
             resultMultiplier = 1;
-        }        
+        }
         return Bytes.compareTo(b1, offset1, length1, b2, offset2, length2) * resultMultiplier;
     }
-    
+
+    public int compareTo(ImmutableBytesWritable ptr1, ColumnModifier ptr1ColumnModifier, ImmutableBytesWritable ptr2, ColumnModifier ptr2ColumnModifier, PDataType type2) {
+        return compareTo(ptr1.get(), ptr1.getOffset(), ptr1.getLength(), ptr1ColumnModifier, ptr2.get(), ptr2.getOffset(), ptr2.getLength(), ptr2ColumnModifier, type2);
+    }
+
     public abstract int compareTo(Object lhs, Object rhs, PDataType rhsType);
 
     public int compareTo(Object lhs, Object rhs) {
