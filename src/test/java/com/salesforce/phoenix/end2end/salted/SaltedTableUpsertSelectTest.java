2d3f4f8c48f0d7d75c804237b50c68fe387198ce
Tony Huang <tonyh1986@gmail.com>
2013-05-14 22:29:53 -0700

    SaltedTableVarLengthRowKeyTest

--- src/test/java/com/salesforce/phoenix/end2end/salted/SaltedTableUpsertSelectTest.java
+++ src/test/java/com/salesforce/phoenix/end2end/salted/SaltedTableUpsertSelectTest.java
@@ -1,5 +1,51 @@
 package com.salesforce.phoenix.end2end.salted;
 
-public class SaltedTableUpsertSelectTest {
+import static com.salesforce.phoenix.util.TestUtil.TEST_PROPERTIES;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import com.salesforce.phoenix.end2end.BaseHBaseManagedTimeTest;
+
+
+public class SaltedTableUpsertSelectTest extends BaseHBaseManagedTimeTest {
+
+    @Test
+    public void testUpsertIntoSaltedTableFromNormalTable() throws Exception {
+        Properties props = new Properties(TEST_PROPERTIES);
+        Connection conn = DriverManager.getConnection(getUrl(), props);
+        conn.setAutoCommit(false);
+        try {
+            
+        } finally {
+            conn.close();
+        }
+    }
+
+    @Test
+    public void testUpsertIntoNormalTableFromSaltedTable() throws Exception {
+        Properties props = new Properties(TEST_PROPERTIES);
+        Connection conn = DriverManager.getConnection(getUrl(), props);
+        conn.setAutoCommit(false);
+        try {
+            
+        } finally {
+            conn.close();
+        }
+    }
+
+    @Test
+    public void testUpsertSaltedTableIntoSaltedTable() throws Exception {
+        Properties props = new Properties(TEST_PROPERTIES);
+        Connection conn = DriverManager.getConnection(getUrl(), props);
+        conn.setAutoCommit(false);
+        try {
+            
+        } finally {
+            conn.close();
+        }
+    }
 }
