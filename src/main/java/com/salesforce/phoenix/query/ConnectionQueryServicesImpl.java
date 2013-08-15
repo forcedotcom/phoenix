1b97504ad1842aa49845d9386c308702eec64c27
James Taylor <jtaylor@salesforce.com>
2013-08-15 15:16:08 -0700

    Use new util method for compat check

--- src/main/java/com/salesforce/phoenix/query/ConnectionQueryServicesImpl.java
+++ src/main/java/com/salesforce/phoenix/query/ConnectionQueryServicesImpl.java
@@ -705,12 +705,7 @@ public class ConnectionQueryServicesImpl extends DelegateQueryServices implement
         if (serverVersion == null) {
             return false;
         }
-        long version = Math.abs(serverVersion);
-        long pversion = MetaDataUtil.decodePhoenixVersion(version);
-        // A server and client with the same major and minor version number must be compatible.
-        // So it's important that we roll the PHOENIX_MAJOR_VERSION or PHOENIX_MINOR_VERSION
-        // when we make an incompatible change.
-        return MetaDataUtil.encodeMaxPatchVersion(MetaDataProtocol.PHOENIX_MAJOR_VERSION, MetaDataProtocol.PHOENIX_MINOR_VERSION) >= pversion;
+        return MetaDataUtil.areClientAndServerCompatible(serverVersion);
     }
 
     private void checkClientServerCompatibility() throws SQLException {
