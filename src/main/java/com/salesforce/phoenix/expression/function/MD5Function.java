5a7c35b33d486702fd346a1ef99a4e0467e7f545
Matt Walsh <matthew.walsh@siemens.com>
2013-09-13 16:33:03 -0500

    Changes to make hashing more efficient.

--- src/main/java/com/salesforce/phoenix/expression/function/MD5Function.java
+++ src/main/java/com/salesforce/phoenix/expression/function/MD5Function.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Salesforce.com, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *     Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *     Neither the name of Salesforce.com nor the names of its contributors may 
+ *     be used to endorse or promote products derived from this software without 
+ *     specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 package com.salesforce.phoenix.expression.function;
 
 import java.security.MessageDigest;
@@ -8,58 +35,60 @@ import java.util.List;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 
 import com.salesforce.phoenix.expression.Expression;
-import com.salesforce.phoenix.parse.FunctionParseNode;
+import com.salesforce.phoenix.parse.FunctionParseNode.*;
 import com.salesforce.phoenix.schema.PDataType;
 import com.salesforce.phoenix.schema.tuple.Tuple;
 
-@FunctionParseNode.BuiltInFunction(name=MD5Function.NAME,  args={
-        @FunctionParseNode.Argument(allowedTypes={PDataType.VARCHAR})} )
+@BuiltInFunction(name = MD5Function.NAME,  args={@Argument()})
 public class MD5Function extends ScalarFunction {
-    public static final String NAME = "MD5";
+  public static final String NAME = "MD5";
 
-    public MD5Function() {
+  private final MessageDigest messageDigest;
+
+  public MD5Function(List<Expression> children) throws SQLException {
+    super(children);
+    try {
+      messageDigest = MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public boolean evaluate(Tuple tuple, ImmutableBytesWritable ptr) {
+    if (!getChildExpression().evaluate(tuple, ptr)) {
+      return false;
     }
 
-    public MD5Function(List<Expression> children) throws SQLException {
-        super(children);
+    String sourceStr = (String) PDataType.VARCHAR.toObject(ptr, getChildExpression().getColumnModifier());
+
+    if (sourceStr == null) {
+      return true;
     }
 
-    @Override
-    public boolean evaluate(Tuple tuple, ImmutableBytesWritable ptr) {
-        if (!getStrExpression().evaluate(tuple, ptr)) {
-            return false;
-        }
+    // Update the digest value
+    messageDigest.update(ptr.get(), ptr.getOffset(), ptr.getLength());
+    // Get the digest bytes (note this resets the messageDigest as well)
+    ptr.set(messageDigest.digest());
+    return true;
+  }
 
-        String sourceStr = (String)PDataType.VARCHAR.toObject(ptr, getStrExpression().getColumnModifier());
+  @Override
+  public PDataType getDataType() {
+    return PDataType.BINARY;
+  }
 
-        if (sourceStr == null) {
-            return true;
-        }
+  @Override
+  public boolean isNullable() {
+    return getChildExpression().isNullable();
+  }
 
-        try {
-          ptr.set(MessageDigest.getInstance("MD5").digest(sourceStr.getBytes()));
-          return true;
-        } catch (NoSuchAlgorithmException e) {
-          throw new IllegalArgumentException(e);
-        }
-    }
+  @Override
+  public String getName() {
+    return NAME;
+  }
 
-    @Override
-    public PDataType getDataType() {
-        return getStrExpression().getDataType();
-    }
-
-    @Override
-    public boolean isNullable() {
-        return getStrExpression().isNullable();
-    }
-
-    @Override
-    public String getName() {
-        return NAME;
-    }
-
-    private Expression getStrExpression() {
-        return children.get(0);
-    }
+  private Expression getChildExpression() {
+    return children.get(0);
+  }
 }
--- src/test/java/com/salesforce/phoenix/end2end/MD5FunctionTest.java
+++ src/test/java/com/salesforce/phoenix/end2end/MD5FunctionTest.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2013, Salesforce.com, Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *     Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *     Neither the name of Salesforce.com nor the names of its contributors may 
+ *     be used to endorse or promote products derived from this software without 
+ *     specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
 package com.salesforce.phoenix.end2end;
 
 import static org.junit.Assert.*;
@@ -12,22 +39,40 @@ import org.junit.Test;
 public class MD5FunctionTest extends BaseHBaseManagedTimeTest {
   
   @Test
-  public void testReverse() throws Exception {
+  public void testRetrieve() throws Exception {
       String testString = "mwalsh";
       
       Connection conn = DriverManager.getConnection(getUrl());
-      String ddl = "CREATE TABLE IF NOT EXISTS MD5_TEST (pk VARCHAR NOT NULL PRIMARY KEY)";
+      String ddl = "CREATE TABLE IF NOT EXISTS MD5_RETRIEVE_TEST (pk VARCHAR NOT NULL PRIMARY KEY)";
       conn.createStatement().execute(ddl);
-      String dml = String.format("UPSERT INTO MD5_TEST VALUES('%s')", testString);
+      String dml = String.format("UPSERT INTO MD5_RETRIEVE_TEST VALUES('%s')", testString);
       conn.createStatement().execute(dml);
       conn.commit();
       
-      ResultSet rs;
-      rs = conn.createStatement().executeQuery("SELECT MD5(pk) FROM MD5_TEST");
+      ResultSet rs = conn.createStatement().executeQuery("SELECT MD5(pk) FROM MD5_RETRIEVE_TEST");
       assertTrue(rs.next());
-      String first = new String(MessageDigest.getInstance("MD5").digest(testString.getBytes()));
-      String second = rs.getString(1);
-      assertEquals(first, second);
+      byte[] first = MessageDigest.getInstance("MD5").digest(testString.getBytes());
+      byte[] second = rs.getBytes(1);
+      assertArrayEquals(first, second);
+      assertFalse(rs.next());
+  }      
+  
+  @Test
+  public void testUpsert() throws Exception {
+      String testString = "mwalsh";
+      
+      Connection conn = DriverManager.getConnection(getUrl());
+      String ddl = "CREATE TABLE IF NOT EXISTS MD5_UPSERT_TEST (pk binary(16) NOT NULL PRIMARY KEY)";
+      conn.createStatement().execute(ddl);
+      String dml = String.format("UPSERT INTO MD5_UPSERT_TEST VALUES(md5('%s'))", testString);
+      conn.createStatement().execute(dml);
+      conn.commit();
+      
+      ResultSet rs = conn.createStatement().executeQuery("SELECT pk FROM MD5_UPSERT_TEST");
+      assertTrue(rs.next());
+      byte[] first = MessageDigest.getInstance("MD5").digest(testString.getBytes());
+      byte[] second = rs.getBytes(1);
+      assertArrayEquals(first, second);
       assertFalse(rs.next());
   }                                                           
 
