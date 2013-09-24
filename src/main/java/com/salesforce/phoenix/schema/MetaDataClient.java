8273e624ce4cbc25d06f820acbff49d906ca2cc0
mravi <maghamravikiran@gmail.com>
2013-09-24 22:11:38 +0530

    modifications to accept lowercase key property

--- src/main/java/com/salesforce/phoenix/schema/MetaDataClient.java
+++ src/main/java/com/salesforce/phoenix/schema/MetaDataClient.java
@@ -560,10 +560,11 @@ public class MetaDataClient {
                 
                 Collection<Pair<String,Object>> props = statement.getProps().get(QueryConstants.ALL_FAMILY_PROPERTIES_KEY);
                 for (Pair<String,Object> prop : props) {
-                    if (defaultDescriptor.getValue(prop.getFirst()) == null) {
-                        tableProps.put(prop.getFirst(), prop.getSecond());
+                	final String keyProperty = SchemaUtil.normalizeIdentifier( prop.getFirst() );
+                    if (defaultDescriptor.getValue(keyProperty) == null) {
+                        tableProps.put(keyProperty, prop.getSecond());
                     } else {
-                        commonFamilyProps.put(prop.getFirst(), prop.getSecond());
+                        commonFamilyProps.put(keyProperty, prop.getSecond());
                     }
                 }
             }
