f01e302edbc24e655680f73ed2858de2b69ffe9b
Jesse Yates <jesse.k.yates@gmail.com>
2013-10-01 18:05:06 -0700

    Adding more threads per htable writer

--- src/main/java/com/salesforce/hbase/index/write/ParallelWriterIndexCommitter.java
+++ src/main/java/com/salesforce/hbase/index/write/ParallelWriterIndexCommitter.java
@@ -82,6 +82,20 @@ public class ParallelWriterIndexCommitter implements IndexCommitter {
   private static final int DEFAULT_CONCURRENT_INDEX_WRITER_THREADS = 10;
   private static final String INDEX_WRITER_KEEP_ALIVE_TIME_CONF_KEY =
       "index.writer.threads.keepalivetime";
+  /**
+   * Maximum number of threads to allow per-table when writing. Each writer thread (from
+   * {@link #NUM_CONCURRENT_INDEX_WRITER_THREADS_CONF_KEY}) has a single HTable. However, each table
+   * is backed by a threadpool to manage the updates to that table. this specifies the number of
+   * threads to allow in each of those tables. Generally, you shouldn't need to change this, unless
+   * you have a small number of indexes to which most of the writes go. Defaults to:
+   * {@value #DEFAULT_NUM_PER_TABLE_THREADS}. For tables to which there are not a lot of writes, the
+   * thread pool automatically will decrease the number of threads to one (though it can burst up to
+   * the specified max for any given table), so increasing this to meet the max case is reasonable.
+   */
+  // TODO per-index-table thread configuration
+  private static final String INDEX_WRITER_PER_TABLE_THREADS_CONF_KEY =
+      "index.writer.threads.pertable.max";
+  private static final int DEFAULT_NUM_PER_TABLE_THREADS = 1;
 
   private ListeningExecutorService writerPool;
   private HTableFactory factory;
@@ -111,9 +125,11 @@ public class ParallelWriterIndexCommitter implements IndexCommitter {
   public static HTableFactory getDefaultDelegateHTableFactory(CoprocessorEnvironment env) {
     // create a simple delegate factory, setup the way we need
     Configuration conf = env.getConfiguration();
-    // only have one thread per table - all the writes are already batched per table.
-
-    conf.setInt("hbase.htable.threads.max", 1);
+    // set the number of threads allowed per table.
+    int htableThreads =
+        conf.getInt(INDEX_WRITER_PER_TABLE_THREADS_CONF_KEY, DEFAULT_NUM_PER_TABLE_THREADS);
+    LOG.info("Starting index writer with " + htableThreads + " threads for each HTable.");
+    conf.setInt("hbase.htable.threads.max", htableThreads);
     return new CoprocessorHTableFactory(env);
   }
 
@@ -128,6 +144,7 @@ public class ParallelWriterIndexCommitter implements IndexCommitter {
     if (maxThreads == 0) {
       maxThreads = 1; // is there a better default?
     }
+    LOG.info("Starting writer with " + maxThreads + " threads for all tables");
     long keepAliveTime = conf.getLong(INDEX_WRITER_KEEP_ALIVE_TIME_CONF_KEY, 60);
 
     // we prefer starting a new thread to queuing (the opposite of the usual ThreadPoolExecutor)
