9e52b3efa855ee1ff635ec929fcc87ab59df31e8
Arun Singh <singh16.arun@gmail.com>
2013-10-23 22:08:58 -0700

    Merging with master

--- src/main/java/com/salesforce/phoenix/map/reduce/MapReduceJob.java
+++ src/main/java/com/salesforce/phoenix/map/reduce/MapReduceJob.java
@@ -30,39 +30,19 @@ package com.salesforce.phoenix.map.reduce;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
-import au.com.bytecode.opencsv.CSVReader;
+import java.sql.*;
+import java.util.*;
 
 import org.apache.commons.lang.StringUtils;
-<<<<<<< HEAD
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-=======
->>>>>>> 6dd198a61419797359d9e291fd05d48202876045
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.util.Pair;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 
-<<<<<<< HEAD
 import au.com.bytecode.opencsv.CSVReader;
 
-=======
->>>>>>> 6dd198a61419797359d9e291fd05d48202876045
 import com.salesforce.phoenix.schema.PDataType;
 import com.salesforce.phoenix.util.PhoenixRuntime;
 import com.salesforce.phoenix.util.QueryUtil;
@@ -109,14 +89,11 @@ public class MapReduceJob {
 				
 				schemaName			= context.getConfiguration().get("schemaName");
 				tableName 			= context.getConfiguration().get("tableName");
-				createDDL[0]		= context.getConfiguration().get("createTableSQL");
-				createDDL[1]		= context.getConfiguration().get("createIndexSQL");
+				createDDL[0]		= context.getConfiguration().get("createTableDDL");
+				createDDL[1]		= context.getConfiguration().get("createIndexDDL");
 				ignoreUpsertError 	= context.getConfiguration().get("IGNORE.INVALID.ROW").equalsIgnoreCase("0") ? false : true;
 				
 				for(String s : createDDL){
-					if(s == null || s.trim().length() == 0)
-						continue;
-
 					try {
 						PreparedStatement prepStmt = conn_none.prepareStatement(s);
 						prepStmt.execute();
@@ -139,7 +116,7 @@ public class MapReduceJob {
 				for(i = 0 ; i < colDetails.size() ; i++){
 					cols.add("?");
 					String prepValues = StringUtils.join(cols, ",");
-					String upsertStmt = "";
+					String upsertStmt = ""; 
 					if(schemaName != null && schemaName.trim().length() > 0)
 						upsertStmt = "upsert into " + schemaName + "." + tableName + " values (" + prepValues + ")";
 					else
@@ -170,14 +147,8 @@ public class MapReduceJob {
 		@Override
 		public void map(LongWritable key, Text line, Context context) throws IOException, InterruptedException{
 			
-<<<<<<< HEAD
-			CSVReader reader = null;
+			CSVReader reader = new CSVReader(new InputStreamReader(new ByteArrayInputStream(line.toString().getBytes())), ',');			
 			try {
-				reader = new CSVReader(new InputStreamReader(new ByteArrayInputStream(line.toString().getBytes())), ',');
-=======
-            CSVReader reader = new CSVReader(new InputStreamReader(new ByteArrayInputStream(line.toString().getBytes())), ',');
-			try {
->>>>>>> 6dd198a61419797359d9e291fd05d48202876045
 				String[] tokens = reader.readNext();
 				
 				PreparedStatement upsertStatement;
@@ -190,7 +161,7 @@ public class MapReduceJob {
 				}
 
 				for(int i = 0 ; i < tokens.length && i < colDetails.size() ;i++){
-					upsertStatement.setObject(i+1, convertTypeSpecificValue(tokens[i], colDetails.get(Integer.valueOf(i+1))));
+					upsertStatement.setObject(i+1, convertTypeSpecificValue(tokens[i], colDetails.get(new Integer(i+1))));
 				}
 				
 				upsertStatement.execute();
@@ -201,17 +172,15 @@ public class MapReduceJob {
 				}
 			} catch (Exception e) {
 				System.err.println("Failed to upsert data in the Phoenix :: " + e.getMessage());
-			} finally{
-				if(reader != null) reader.close();
-			}
+			}finally {
+				reader.close();
+       			} 
 			
 			Iterator<Pair<byte[],List<KeyValue>>> dataIterator = null;
 			try {
 				dataIterator = PhoenixRuntime.getUncommittedDataIterator(conn_none);
 			} catch (SQLException e) {
 				System.err.println("Failed to retrieve the data iterator for Phoenix table :: " + e.getMessage());
-			} finally {
-			    reader.close();
 			}
 			
 			while(dataIterator != null && dataIterator.hasNext()){
